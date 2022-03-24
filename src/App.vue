f135df01779426517a0895415d25c24b4b834380
Fernanda Avello <fernandaavellorocha@gmail.com>
2022-03-24 17:03:39 -0300

    Agregar Google-font

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,13 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/hamburguesas">Hamburguesas</router-link>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Lato', "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -27,6 +28,4 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
-
-@import "~bootstrap/dist/css/bootstrap.css"
 </style>
