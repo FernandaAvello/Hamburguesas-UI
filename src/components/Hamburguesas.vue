cd986ee974e247b1ef3722a823b8217492100d13
Fernanda Avello <fernandaavellorocha@gmail.com>
2022-03-24 09:59:37 -0300

    Inicio del Proyecto

---
+++ src/components/Hamburguesas.vue
@@ -0,0 +1,38 @@
+<template>
+   <div>
+       <table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Nombre Hamburguesa</th>
+      <th scope="col">Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+    </tr>
+  </tbody>
+</table>
+</div> 
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Burgers.vue
@@ -0,0 +1,26 @@
+<template>
+
+<Hamburguesas />
+
+  <div>
+    <h1>Completa aquí el desafío</h1>
+
+  </div>
+</template>
+
+<script>
+import Hamburguesas from "@/components/Hamburguesas.vue"
+
+
+
+export default {
+  name: 'Burgers',
+  components: {
+    Hamburguesas
+  }
+}
+</script>
+
+<style>
+    
+</style>
