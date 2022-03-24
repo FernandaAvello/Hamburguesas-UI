49e15a788364bf9810d47bfb7562c324ea4abd82
Fernanda Avello <fernandaavellorocha@gmail.com>
2022-03-24 17:04:44 -0300

    Importar font-awesome + config libreria iconos, bootstrap css

--- src/main.js
+++ src/main.js
@@ -4,10 +4,18 @@ import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPencil, faTrashCan, faEye, faPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-
+library.add(faPencil)
+library.add(faTrashCan)
+library.add(faEye)
+library.add(faPlus)
 
 const app = createApp(App)
 app.use(router)
 app.use(VueAxios, axios)
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
